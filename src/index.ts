4473499351e9e4ad1b815a22de91386565744bad
unknown <jamesonheadley@gmail.com>
2025-02-13 13:17:35 -0500

    Update code: Added Truck and Motorbike classes and updated CLI functionality

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,24 @@ import Cli from "./classes/Cli.js";
 const vehicles = [];
 
 // Create a truck (using default wheels if an empty array is passed)
-const truck1 = new Truck(Cli.generateVin(), "red", "Ford", "F-150", 2021, 5000, 120, [], 10000);
+const truck1 = new Truck(
+  Cli.generateVin(),
+  "red",
+  "Ford",
+  "F-150",
+  2021,
+  5000,
+  120,
+  [],
+  10000
+);
 
 // Create a car (the Car class will use 4 default wheels if an empty array is passed)
 const car1 = new Car(
   Cli.generateVin(),
-  'blue',
-  'Toyota',
-  'Camry',
+  "blue",
+  "Toyota",
+  "Camry",
   2021,
   3000,
   130,
@@ -25,7 +35,16 @@ const car1 = new Car(
 
 // Create a motorbike with 2 wheels
 const motorbike1Wheels = [new Wheel(17, "Michelin"), new Wheel(17, "Michelin")];
-const motorbike1 = new Motorbike(Cli.generateVin(), "black", "Harley Davidson", "Sportster", 2021, 500, 125, motorbike1Wheels);
+const motorbike1 = new Motorbike(
+  Cli.generateVin(),
+  "black",
+  "Harley Davidson",
+  "Sportster",
+  2021,
+  500,
+  125,
+  motorbike1Wheels
+);
 
 // Push vehicles to the array
 vehicles.push(truck1);
